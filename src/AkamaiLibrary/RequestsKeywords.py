87159d7650da937c14c53f8027926df4b6852815
Anthony Hogg <anthony@hogg.fr>
2020-06-15 15:33:41 +0200

    add keyword: Set Session Pragma Header

--- src/AkamaiLibrary/RequestsKeywords.py
+++ src/AkamaiLibrary/RequestsKeywords.py
@@ -1,4 +1,5 @@
 from RequestsLibrary import RequestsLibrary
+from .pragmas import DEFAULT_PRAGMAS
 
 # Avoid Python http error when response contains more than 100 headers.
 # This is common when using Pragma: akamai-x-get-extracted-values.
@@ -12,6 +13,7 @@ class RequestsKeywords(RequestsLibrary):
 
     Additionally, provides the following convenience keywords:
 
+    - ``[#Set Session Pragma Header|Set Session Pragma Header]``
     - ``[#Set Session Header|Set Session Header]``
     - ``[#Unset Session Header|Unset Session Header]``
     - ``[#Set Session Cookie|Set Session Cookie]``
@@ -23,7 +25,7 @@ class RequestsKeywords(RequestsLibrary):
         Shortcut to add a header to all subsequent requests made over the session.
 
         Examples:
-        | Set Session Header | Authorization | Basic xyz |
+        | Set Session Header | session | Authorization | Basic xyz |
         """
         self.update_session(session, headers={name: value})
 
@@ -32,7 +34,7 @@ class RequestsKeywords(RequestsLibrary):
         Removes a header from all subsequent requests made over the session.
 
         Examples:
-        | Set Session Header | Authorization |
+        | Set Session Header | session | Authorization |
         """
         self.set_session_header(session, name, None)
 
@@ -41,7 +43,7 @@ class RequestsKeywords(RequestsLibrary):
         Shortcut to add a cookie to all subsequent requests made over the session.
 
         Examples:
-        | Set Session Cookie | SESSIONID | xyz |
+        | Set Session Cookie | session | SESSIONID | xyz |
         """
         self.update_session(session, cookies={name: value})
 
@@ -50,6 +52,23 @@ class RequestsKeywords(RequestsLibrary):
         Removes a cookie from all subsequent requests made over the session.
 
         Examples:
-        | Set Session Cookie | SESSIONID |
+        | Set Session Cookie | session | SESSIONID |
         """
         self.set_session_cookie(session, name, None)
+
+    def set_session_pragma_header(self, session, *pragmas):
+        """
+        Add pragma headers to the session.
+
+        Specific pragmas can be added by listing them explicitly.
+        If omitted, the default set of pragmas will be used.
+
+        {DEFAULT_PRAGMAS}
+
+        Examples:
+        | Set Session Pragma Header | session | akamai-x-get-request-id | akamai-x-get-cache-key |
+        | Set Session Pragma Header | session |
+        """
+        if len(pragmas) == 0:
+            pragmas = DEFAULT_PRAGMAS
+        self.set_session_header(session, "pragma", ",".join(pragmas))
---
+++ src/AkamaiLibrary/pragmas.py
@@ -0,0 +1,11 @@
+DEFAULT_PRAGMAS = (
+    "akamai-x-get-request-id",
+    "akamai-x-get-cache-key",
+    "akamai-x-get-true-cache-key",
+    "akamai-x-get-extracted-values",
+    "akamai-x-cache-on",
+    "akamai-x-cache-remote-on",
+    "akamai-x-check-cacheable",
+    "akamai-x-get-ssl-client-session-id",
+    "akamai-x-serial-no",
+)
